d00cff6c0d0770162b00f15e181cb08936f00afb
wanshenggang <wanshenggang@beisen.com>
2016-01-16 10:04:10 +0800

    ss

--- src/common/components/ui/sidebar/Sidebar.js
+++ src/common/components/ui/sidebar/Sidebar.js
@@ -2,16 +2,29 @@ import React, { Component, PropTypes } from 'react';
 import HotTopic from  './HotTopic'
 
 export default class Sidebar extends Component {
+
+    _getHotTopics(hotTopics){
+             return hotTopics.map((arr,index)=>{
+                return <HotTopic {...arr} key={index}/>
+            })
     
+    }
     render(){
         
+         var dataArr=[{title:"222",answers:10,isSolved:true,href:"222.com"}
+                    ,{title:"222",answers:10,isSolved:true,href:"222.com"}
+                    ,{title:"222",answers:10,isSolved:true,href:"222.com"}
+                    ,{title:"222",answers:10,isSolved:true,href:"222.com"}
+                    ]
+        
+
         return (
             <div className="col-xs-12 col-md-3 side mt30">
             <div className="widget-box">
                     <h2 className="h4 widget-box__title">最近热门的</h2>
                     <ul className="widget-links list-unstyled">
                         
-                        <HotTopic data={this.props.data}/>
+                        {this._getHotTopics(dataArr)}
                     </ul>
                 </div>
                 </div>
