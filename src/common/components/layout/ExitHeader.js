4e36e1923adef7116e0fcbd133a56fb743b98c82
suxiaohui <suxiaohui@beisen.com>
2016-01-17 15:17:31 +0800

    add menu exit

--- src/common/components/layout/ExitHeader.js
+++ src/common/components/layout/ExitHeader.js
@@ -1,1 +1,26 @@
-ExitHeader.js
+import React,{Component,PropTypes} from 'react';
+export default class EixtWidget extends Component {
+	constructor (props) {
+		super (props);		
+	}
+	mouseover () {		
+		$(".exit-menu").show();	
+	}
+	mouseout () {	
+		$(".exit-menu").hide();	
+		
+	}
+	render () {		
+		return (
+			<div className="exitBox" onMouseOver={this.mouseover.bind(this)} onMouseOut={this.mouseout.bind(this)}>
+				<span className="photoMenu"></span>
+				<div className="exit-menu">
+					<ul className="exit-content">
+						<li><a href="#">我的主页</a></li>
+						<li><a href="#">退出</a></li>
+					</ul>
+				</div>
+			</div>
+		);
+	}
+}
